7fdeb25fcdfbc0db6b63f6bea31f78ccc245bce2
sergey_chernov <chernov.sergey23@gmail.com>
2020-11-22 19:31:03 +0200

    Update client.go

--- client/client.go
+++ client/client.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
+	"flag"
 	"log"
+	"sync/atomic"
 	"time"
 
 	"github.com/chernovsergey/dockerized_service/api"
@@ -10,7 +12,7 @@ import (
 )
 
 const (
-	port    = "localhost:5050"
+	port    = "192.168.0.106:5050"
 	text    = "banana"
 	pattern = "ana"
 )
@@ -25,13 +27,14 @@ func precalc(client api.FinderClient, timeout time.Duration,
 
 func lookup(client api.FinderClient, timeout time.Duration,
 	pattern string) (*api.Offsets, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), timeout)
-	defer cancel()
-
+	//ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	ctx := context.Background()
 	return client.Lookup(ctx, &api.Pattern{Value: pattern})
 }
 
 func main() {
+	nWorkers := flag.Int("workers", 100, "-w")
+	flag.Parse()
 
 	// Create connection to grpc server, working on
 	// specific adress
@@ -52,18 +55,48 @@ func main() {
 
 	// Estimate rps
 	log.Println("start")
-	success := 0
-	go func() {
-		for {
-			if _, err := lookup(client, time.Second, pattern); err == nil {
-				success++
+
+	done := make(chan int32, 100000)
+	errs := make(chan int32, 100000)
+
+	workers := *nWorkers
+	log.Println("Using %v workers", workers)
+
+	for i := 0; i < workers; i++ {
+		go func() {
+			for {
+				if _, err := lookup(client, 200 * time.Millisecond, pattern); err == nil {
+					done <- 1
+				} else {
+					errs <- 1
+				}
 			}
-		}
-	}()
+		}()
+	}
+
+	var success int32
+	for i := 0; i < workers; i++ {
+		go func() {
+			for v := range done{
+				atomic.AddInt32(&success, v)
+			}
+		}()
+	}
+
+	var errors int32
+	for i := 0; i < workers; i++ {
+		go func() {
+			for v := range errs{
+				atomic.AddInt32(&errors, v)
+			}
+		}()
+	}
 
 	select {
-	case <-time.After(60 * time.Second):
-		log.Println("stop")
-		log.Printf("Estimated requests per second = %v", success)
+	case <-time.After(10 * time.Second):
+		close(done)
+		close(errs)
+		log.Println("Done %v requests", success)
+		log.Println("Errs %v requests", errors)
 	}
 }
